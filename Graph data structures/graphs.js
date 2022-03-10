1b1e5dd58b3727a86bd8be5d4739979687dfd440
Fahdroussafi <cmpunk589@outlook.com>
2022-03-10 11:23:24 +0100

    add and show nodes

--- Graph data structures/graphs.js
+++ Graph data structures/graphs.js
@@ -21,32 +21,37 @@ class Graph {
         if (!this.#nodes[destination].includes(source)){
             this.#nodes[destination].push(source)
         }
-    }
 
+      }  
+        showNodes() {
+            console.log(this.#nodes);
+        }
+}
+    
 /**
     Vova -   Tim        Jeff
     |         |    \      |
     John -   Ann - Lee - Ron
     |
 */
-let g = newGraph();
+let g = new Graph();
 
+g.addNode ('Vova');
+g.addNode ('Tim');
+g.addNode ('John');
+g.addNode ('Ann');
+g.addNode ('Ron');
+g.addNode ('Jeff');
 
+g.addEdge('Tim','Vova');
+g.addEdge('Tim','Ann');
+g.addEdge('Tim','Lee');
+g.addEdge('Ann','John');
+g.addEdge('Ann','Lee');
+g.addEdge('Ron','Lee');
+g.addEdge('Ron','Lee');
+g.addEdge('Ron','Jeff');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+g.showNodes();
+// console.log(g.dfs('Tim','Jeff'));
+// console.log(g.bfs('Tim','Jeff'));
---
+++ Graph data structures/hello.js
@@ -0,0 +1,2 @@
+const x = "my first app";
+console.log(x);
